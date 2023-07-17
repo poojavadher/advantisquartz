751a3f5086768338eeb86ad5100fdad6e0747b92
poojavadher24 <poojavadher24@gmail.com>
2023-07-17 14:39:05 +0530

    update PO print format

---
+++ advantisquartz/advantisquartz/doctype/serial_no_update_field/serial_no_update_field.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, pooja@sanskartechnolab.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Serial No Update Field', {
+	// refresh: function(frm) {
+
+	// }
+});
