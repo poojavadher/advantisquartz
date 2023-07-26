c14272b5aedf53771a89bbe68c638cf0546e1b82
poojavadher24 <poojavadher24@gmail.com>
2023-07-26 10:59:30 +0530

    Add Purchase Reports

---
+++ advantisquartz/advantisquartz/doctype/exit_interview_questionnaire/exit_interview_questionnaire.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, pooja@sanskartechnolab.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Exit Interview Questionnaire', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ advantisquartz/advantisquartz/doctype/out_door_duty/test_out_door_duty.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, pooja@sanskartechnolab.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestOutDoorDuty(FrappeTestCase):
+	pass
